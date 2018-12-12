1a14f011b91d1ed7fd35cd85bf858ddcaf1f796e
Anton Kogan <34128946+chinhippy@users.noreply.github.com>
2018-12-11 22:16:07 -0600

    npm packages and initial arguments

--- liri.js
+++ liri.js
@@ -1,3 +1,15 @@
+// Imported npm packages 
 require("dotenv").config();
 const keys = require("./keys.js");
+const moment = require("moment");
+const fs = require("fs");
+const axios = require("axios");
+
+// Spotify ID & Secret in the .env file and keys.js file
+const sp = require("node-spotify-api");
+let spotify = new sp(keys.spotify);
+
+// Variables for user input in command line
+let argument = process.argv[2];
+let input = process.argv[3];
 
